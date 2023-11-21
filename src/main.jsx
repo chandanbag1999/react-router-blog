1a75cd97d4ea4e5994bb29bd3b8d1c73682f73a0
Chandan Bag <cbag67612@gmail.com>
2023-11-21 22:25:57 +0530

    some changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,32 @@
-// main.jsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Layout from './Layout.jsx';
-import Home from './Component/Home/Home.jsx';
-import Contact from './Component/Contact/Contact.jsx';
-import User from './Component/User/User.jsx';
-import Github, { GithubLoader } from './Component/Github/Github.jsx';
-import Regarding from './Component/Regarding/Regarding.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './Component/Home/Home.jsx'
+import Contact from './Component/Contact/Contact.jsx'
+import User from './Component/User/User.jsx'
+import Github, { GithubLoader } from './Component/Github/Github.jsx'
+import Regarding from './Component/Regarding/Regarding.jsx'
+// import Price from './Component/Price/Price.jsx'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+    <Route path='' element={<Home/>}/>
+    <Route path='regarding' element={<Regarding/>}/>
+    <Route path='contact' element={<Contact/>}/>
+    <Route path='user/:userid' element={<User/>}/>
+    <Route path='github' element={<Github/>} loader={GithubLoader}/>
+
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path='/' element={<Layout/>}>
-          <Route index element={<Home/>} />
-          <Route path='regarding' element={<Regarding/>} />
-          <Route path='contact' element={<Contact/>} />
-          <Route path='user/:userid' element={<User/>} />
-          <Route path='github' element={<Github/>} loader={GithubLoader} />
-        </Route>
-      </Routes>
-    </Router>
+    <RouterProvider router={router}/>
   </React.StrictMode>,
-);
+)
